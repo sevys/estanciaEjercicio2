531c31fa3053bb76c2e3bdb3c081e479f0e145c0
sevys <201228@ids.upchiapas.edu.mx>
2021-08-04 01:08:24 -0500

    eliminando carpetas

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module estanciaEjercicio2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires java.desktop;
-
-    opens sample;
-    opens sample.controller;
-}
